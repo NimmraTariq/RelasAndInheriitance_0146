2eee82bd522e641c6d93aae0840d0b61661946af
NimmraTariq <nimra369tariq@gmail.com>
2025-05-26 18:51:43 +0700

    Adds "rini" as a child to "dini"

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -9,6 +9,12 @@ int main ()
 {
     ibu *varibu = new ibu ("dini");
     ibu *varibu2 = new ibu ("novi"); 
+    anak *varAnak1 = new anak ("tono");
+    anak *varAnak2 = new anak ("rini");
+    anak *varAnak3 = new anak ("Dewi");
+
+    varibu -> tambahAnak (varAnak1);
+    varibu -> tambahAnak (varAnak2); 
 }
 
 
